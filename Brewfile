6db2455d51d763797945261b2ee2dc015e7dc295
Raghav Mandayam <raghavmandayam@gmail.com>
2018-04-22 02:18:28 -0700

    Install: Consolidated Homebrew dependencies to Brewfile

---
+++ Brewfile
@@ -0,0 +1,7 @@
+cask 'iterm2'
+brew 'macvim', args: ['with-override-system-vim']
+brew 'tmux'
+brew 'reattach-to-user-namespace'
+brew 'pyenv'
+brew 'node'
+brew 'cmake' # necessary for YouCompleteMe
--- install.sh
+++ install.sh
@@ -3,20 +3,12 @@
 export SCRIPT_DIR
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo "Installing iTerm2..."
-brew cask install iterm2
+echo "Installing Homebrew packages from Brewfile..."
+brew bundle
+
+# set iTerm2 preferences load directory
 defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$SCRIPT_DIR/iterm"
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
-
-echo "Installing vim..."
-brew install macvim --with-override-system-vim
-
-echo "Installing tmux..."
-brew install tmux
-brew install reattach-to-user-namespace
-
-echo "Installing pyenv..."
-brew install pyenv
 
 echo "Installing oh-my-zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -26,9 +18,6 @@ cd fonts
 cd ..
 rm -rf fonts
 
-echo "Installing node..."
-brew install node
-
 echo "Installing vim-plug..."
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -37,5 +26,4 @@ echo "Setting up symlinks..."
 source ./symlinks.sh
 
 echo "Installing vim plugins..."
-brew install cmake # necessary for YouCompleteMe
 vim +PlugInstall +qall
