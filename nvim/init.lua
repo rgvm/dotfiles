acfa3059af79535a9214ec7d3e6f0b8e47ac0bdb
Raghav Mandayam <raghavmandayam@gmail.com>
2025-12-27 19:16:07 -0800

    vim: Add some basic settings.

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,21 +14,25 @@ vim.pack.add({
 
 require("oil").setup()
 
+-- basic settings
+vim.opt.cursorline = true
+vim.opt.scrolloff = 8
+vim.opt.wrap = false
+
 -- color
 vim.g.base16colorspace=256  -- Access colors present in 256 colorspace
 vim.cmd('colorscheme base16-gruvbox-dark-medium')
 
 -- indentation
+vim.opt.autoindent = true
 vim.opt.expandtab = true
+vim.opt.smartindent = true
 vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
 vim.api.nvim_create_autocmd("FileType", {
     pattern = { "javascript", "typescript", "html", "c", "cpp", "cc" },
     callback = function()
-        vim.opt_local.autoindent = true
-        vim.opt_local.expandtab = true
-        vim.opt_local.smartindent = true
         vim.opt_local.shiftwidth = 2
         vim.opt_local.softtabstop = 2
         vim.opt_local.tabstop = 2
