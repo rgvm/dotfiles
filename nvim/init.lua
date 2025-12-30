b86f398c1b3616e15bd74615e1d9129f0e28773f
Raghav Mandayam <raghavmandayam@gmail.com>
2025-12-30 00:34:45 -0800

    vim: Map H to ^ and L to $.

--- nvim/init.lua
+++ nvim/init.lua
@@ -118,6 +118,10 @@ vim.keymap.set("n", "<C-k>", "<C-w><C-k>")
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>")
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>")
 
+-- beginning/end of line remaps
+vim.keymap.set({ "n", "v", "o" }, "H", "^")
+vim.keymap.set({ "n", "v", "o" }, "L", "$")
+
 -- visual mode line moving
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
