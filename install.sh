cfd49ac2546f8714ca9b1919122063b85214b768
Raghav Mandayam <raghavmandayam@gmail.com>
2026-01-04 23:36:32 -0800

    iTerm: Remove base16 shell dep.
    This also seems to be the culprit for the weird text that appears when
    opening terminal from vim.

--- install.sh
+++ install.sh
@@ -13,9 +13,6 @@ echo "Configuring iTerm2 preferences..."
 defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$SCRIPT_DIR/iterm"
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 
-echo "Installing Base16 Shell..."
-git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
-
 echo "Configuring Neovim..."
 mkdir -p ~/.config/nvim
 
